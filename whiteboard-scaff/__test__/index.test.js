87137113408f9459a259c5d7134f04b2605e8364
Jeremy <jpjazzy@hotmail.com>
2018-01-24 07:32:04 -0800

    added a scaffold for whiteboard challenges

---
+++ whiteboard-scaff/__test__/index.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+const whiteboardFn = require('../index')
+
+
+describe('#null Case ', function() {
+    it('Should return: null', function() {
+        expect(whiteboardFn.).toBeUndefined();
+    });
+});
+
+
+describe('#Standard Case ', function() {
+    it('Should return: ', function() {
+        expect(whiteboardFn.).toBeUndefined();
+    });
+});
